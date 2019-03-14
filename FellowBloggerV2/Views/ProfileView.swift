b3dbcddd73ef84e65e95bdf6766e3b81af21aaa5
onielrosario <onielrosario@pursuit.org>
2019-03-14 18:14:54 -0400

    creating profile view

---
+++ FellowBloggerV2/Views/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  FellowBloggerV2
+//
+//  Created by Oniel Rosario on 3/14/19.
+//  Copyright © 2019 Oniel Rosario. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
